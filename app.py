6d10d12e7df8a4bade08f74cd54b7dff491d5f60
Elodie <elo.schwartz91@gmail.com>
2026-03-22 21:07:25 +0100

    ajout de la bar chart de l'inventaire sur le dashboard streamlit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 from fruit_manager import *
 
 st.title("🍇​ Dashboard de la Plantation")
@@ -11,7 +12,18 @@ st.header("💰​ Trésorerie")
 st.metric(label="Montant disponible", value=f"{tresorerie:.2f} $")
 
 st.header("📦​ Inventaire")
+# Inventaire sous forme de tableau
 st.table(inventaire)
+# Inventaire sous forme de graphique
+fig, ax = plt.subplots()
+# Trier l'inventaire par quantité croissante
+inventaire = dict(sorted(inventaire.items(), key=lambda item: item[1], reverse=True))
+ax.bar(inventaire.keys(), inventaire.values(), edgecolor="k")
+ax.set_xlabel("Fruit")
+ax.set_ylabel("Quantité")
+ax.set_title("Inventaire")
+st.pyplot(fig)
+
 
 # Barre latérale
 
